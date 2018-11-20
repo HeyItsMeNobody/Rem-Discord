e81f376d432dc04714f4a3446173f652509526c7
HeyItsMeNobody <blomdyon@yahoo.com>
2018-11-20 18:05:15 +0100

    Modified say command C:

--- commands/say.js
+++ commands/say.js
@@ -1,11 +1,30 @@
 module.exports.run = async (client, message, messageArray, cmd, args, config) => {
+    if (args[0] == "channel") {
+        if (!message.member.hasPermission('ADMINISTRATOR')) {
+            message.channel.send(`You don't have the administrator permission`);
+            return;
+        } else {
+            try {
+                var sendMessage = args.splice(2).join(" ");
+                if (args[1] == undefined) {
+                    return message.channel.send(`You didn't give me a channel id to send the message to!`);
+                } else if (sendMessage == "") {
+                    return message.channel.send(`You didn't give me anything to send to <#${args[1]}>!`);
+                } else {
+                    message.guild.channels.get(`${args[1]}`).send(sendMessage);
+                }
+            } catch (err) {
+                return message.channel.send(`Something went wrong, Did you double check the channel id?`);
+            }
+        }
+        return;
+    } 
     const sayMessage = args.join(" ");
     sayMessageFiltered = sayMessage.replace('@', '');
     if (sayMessageFiltered === "") {
         message.channel.send("You didn't give me anything to say uwu")
         return;
-    }
-    else {
+    } else {
         message.delete().catch(O_o=>{});
         message.channel.send(sayMessageFiltered);
         return;
