9f82091210fdff153e1b8bf208e9401862790f96
HeyItsMeNobody <blomdyon@yahoo.com>
2018-11-23 15:28:19 +0100

    >Changed url to https

--- commands/help.js
+++ commands/help.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 module.exports.run = async (client, message, messageArray, cmd, args, config) => {
     const embed = new Discord.RichEmbed()
     embed.setTitle('Help page');
-    embed.setDescription('You can find the help page [here](http://rem.nobody.life/commands.html)');
+    embed.setDescription('You can find the help page [here](https://rem.nobody.life/commands.html)');
     embed.setColor('#BA55D3');
     message.channel.send(embed);
 }
