e799973151dbea76df49397edaa13fd82381b4b5
HeyItsMeNobody <blomdyon@yahoo.com>
2018-11-24 12:11:41 +0100

    Thanking raven for the idea

--- commands/update.js
+++ commands/update.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const { exec } = require('child_process');
 module.exports.run = async (client, message, messageArray, cmd, args, config) => {
+    // Oh btw, Thanks raven for the idea
     if (message.author.id == config.discord.ownerid) {
         if (client.user.id == "508670321835114506") {
             const embed = new Discord.RichEmbed()
