135e0d51e8ffb273e4b5090bddf6a139c8c8f9fd
HeyItsMeNobody <blomdyon@yahoo.com>
2018-12-06 08:52:30 +0100

    Added 27 NSFW commands.. I'm sorry..

---
+++ commands/futa.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+const nekoclient = require('nekos.life');
+const neko = new nekoclient();
+
+module.exports.run = async (client, message, messageArray, cmd, args, config) => {
+    if (message.channel.nsfw) {
+        const embed = new Discord.RichEmbed()
+        await neko.getNSFWFutanari().then((image) => embed.setImage(image.url));
+        embed.setColor('#BA55D3');
+        await message.channel.send(embed);
+    } else {
+        const embed = new Discord.RichEmbed()
+        embed.setTitle(`This isn't a NSFW channel ${message.author.tag}`)
+        embed.setColor('#BA55D3');
+        message.channel.send(embed);
+    }
+}
+
+module.exports.help = {
+    name: "futa"
+}
