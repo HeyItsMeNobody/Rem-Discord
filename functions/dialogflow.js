d79664dfbc5c5311048ded8fc345794374d09def
HeyItsMeNobody <blomdyon@yahoo.com>
2019-01-11 16:45:57 +0100

    Put dialogflow in seperate folder

---
+++ functions/dialogflow.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const { google } = require('googleapis');
+const uuid = require('uuid');
+const config = require('../config.json');
+
+module.exports = {
+    DetectIntent: async function(message) {
+        const gclient = await google.auth.getClient({
+            keyFile: path.resolve('./Rem-Googlecloud.json'),
+            scopes: 'https://www.googleapis.com/auth/cloud-platform'
+        });
+        const dialogflow = google.dialogflow({
+            version: 'v2beta1',
+            auth: gclient
+        });
+
+         const result = await dialogflow.projects.agent.sessions.detectIntent({
+            session: `projects/${config.dialogflow.pId}/agent/sessions/${uuid.v4()}`,
+            requestBody: {
+                queryInput: {
+                    text: {
+                        text: message.content,
+                        languageCode: 'en-US',
+                    },
+                },
+            },
+        });
+
+        message.channel.send(result.data.queryResult.fulfillmentText);
+    }
+}
--- main.js
+++ main.js
@@ -6,8 +6,7 @@ const config = require('./config.json');
 const getConnection = require('./mysqlPool.js');
 const talkedRecently = new Set();
 const path = require('path');
-const { google } = require('googleapis');
-const uuid = require('uuid');
+const dialogflow = require('./functions/dialogflow.js');
 
 client.on('ready', () => {
     console.log(`Loaded and logged in as ${client.user.tag}`);
@@ -48,30 +47,7 @@ client.on('message', async message => {
     if (message.author.bot) return;
 
     if (message.channel.type == "dm") {
-        const gclient = await google.auth.getClient({
-            keyFile: path.resolve('./Rem-Googlecloud.json'),
-            scopes: 'https://www.googleapis.com/auth/cloud-platform'
-        });
-
-        const dialogflow = google.dialogflow({
-            version: 'v2beta1',
-            auth: gclient
-        });
-
-        const result = await dialogflow.projects.agent.sessions.detectIntent({
-            session: `projects/${config.dialogflow.pId}/agent/sessions/${uuid.v4()}`,
-            requestBody: {
-                queryInput: {
-                    text: {
-                        text: message.content,
-                        languageCode: 'en-US',
-                    },
-                },
-            },
-        });
-
-        message.channel.send(result.data.queryResult.fulfillmentText);
-
+        dialogflow.DetectIntent(message);
         return;
     }
 
